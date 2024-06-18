b61f306eb66b6dd1dfdc1e72303deb5095fb6f72
Rhy2468 <rickyxianyang@gmail.com>
2024-06-18 15:40:57 -0700

    added logo, reformatted page.tsx, and changed background color

--- tansen/app/page.tsx
+++ tansen/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
+import tansenLogo from './assets/tansenLogo.png';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        Tansen Izakaya
+      <div className="header">
+        <Image src={tansenLogo} alt="Tansen Izakaya Logo" />
+        <div className="navbar"></div>
       </div>
     </main>
   );
