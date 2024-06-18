3652f7f04eee8f24b0219cf399c55b681e619d07
Rhy2468 <rickyxianyang@gmail.com>
2024-06-18 16:13:08 -0700

    Added home page picture of dishes (need to replace image)

--- tansen/app/page.tsx
+++ tansen/app/page.tsx
@@ -1,12 +1,23 @@
+//Library Imports
 import Image from "next/image";
+import Link from "next/link";
+
+//Images
 import tansenLogo from './assets/tansenLogo.png';
+import mainDishes from './assets/mainDishes.png';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="header">
-        <Image src={tansenLogo} alt="Tansen Izakaya Logo" />
+        <Link href="/">
+          <Image src={tansenLogo} alt="Tansen Izakaya Logo"/>
+        </Link>
         <div className="navbar"></div>
+      </div>
+
+      <div className="mainDishes">
+        <Image src={mainDishes} alt="Picture of popular dishes"/>
       </div>
     </main>
   );
