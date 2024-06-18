bb6aca59fc4a85e1127cc44e322547669e893e02
flora-yc <57274404+flora-yc@users.noreply.github.com>
2024-06-18 16:11:33 -0700

    Added navigation bar with font

--- tansen/app/page.tsx
+++ tansen/app/page.tsx
@@ -3,11 +3,15 @@ import tansenLogo from './assets/tansenLogo.png';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center">
       <div className="header">
         <Image src={tansenLogo} alt="Tansen Izakaya Logo" />
-        <div className="navbar"></div>
       </div>
+      <div className="flex-container">
+          <div> ABOUT US</div>
+          <div> MENU</div>
+          <div> CONTACT US</div>
+        </div>
     </main>
   );
 }
